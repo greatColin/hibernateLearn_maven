90598221c7d36b21c2187880dea4e6e445b881e2
Colin <524083648@qq.com>
2018-01-15 01:12:49 +0800

    add hibernate base control

--- src/main/java/com/e222/demo.java
+++ src/main/java/com/e222/demo.java
@@ -6,7 +6,6 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import com.e222.entities.Article;
-
 import java.util.List;
 
 public class demo {
@@ -22,7 +21,38 @@ public class demo {
         查询：from 查询对象的完整类名(没有重复直接类名)
         条件： from 类名 where 对象属性 = ?
         */
-        Article a = new Article();
+        //Article a = new Article();
+        String hql = " from Article where num = ?";
+        //2.根据HQL语句获取查询对象
+        Query query = session.createQuery(hql);
+        //设置参数
+        query.setParameter(0,1);
+        //3.根据查询对象获取查询结果
+        List<Article> list = query.list();//获取多个对象
+
+        System.out.println("list = " + list.toString());
+//query.uniqueResult();//获取唯一对象
+        //------------------------------------------------------------
+        tx.commit();
+        session.close();
+        sessionFactory.close();
+        baseHibernateCon();
+    }
+    /*
+    * 基本增删改查
+    * */
+    private static void baseHibernateCon(){
+        Configuration conf = new Configuration().configure();
+        SessionFactory sessionFactory = conf.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        //-------------------------HQL操作----------------------------
+
+        //1.书写HQL语句
+        /*
+        查询：from 查询对象的完整类名(没有重复直接类名)
+        条件： from 类名 where 对象属性 = ?
+        */
         String hql = " from Article where num = ?";
         //2.根据HQL语句获取查询对象
         Query query = session.createQuery(hql);
